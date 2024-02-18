c8a779ce99647b143ac0b5b70bf7680caa1b5de6
Varun Sai <varunsai855@gmail.com>
2024-02-17 22:57:06 -0500

    02142024 commit 1

--- src/pages/qcomps/updObjectsForm.js
+++ src/pages/qcomps/updObjectsForm.js
@@ -1,52 +1,57 @@
-import { useState } from 'react';
+import {useState} from 'react';
 
 export default function Scoreboard() {
-  const [player, setPlayer] = useState({
-    firstName: 'Ranjani',
-    lastName: 'Shettar',
-    score: 10,
-  });
+    const [player, setPlayer] = useState({
+                                             firstName: 'Ranjani',
+                                             lastName: 'Shettar',
+                                             score: 10,
+                                         });
 
-  function handlePlusClick() {
-    player.score++;
-  }
+    function handlePlusClick() {
+        const addNum = player.score + 1;
+        setPlayer({
+                      ...player,
+                      score: addNum,
+                  });
+    }
 
-  function handleFirstNameChange(e) {
-    setPlayer({
-      ...player,
-      firstName: e.target.value,
-    });
-  }
+    function handleFirstNameChange(e) {
+        setPlayer({
+                      ...player,
+                      firstName: e.target.value,
+                  });
+    }
 
-  function handleLastNameChange(e) {
-    setPlayer({
-      lastName: e.target.value
-    });
-  }
+    function handleLastNameChange(e) {
+        setPlayer({
+                      ...player,
+                      lastName: e.target.value,
+                  });
+    }
 
-  return (
-    <>
-      <label>
-        Score: <b>{player.score}</b>
-        {' '}
-        <button onClick={handlePlusClick}>
-          +1
-        </button>
-      </label>
-      <label>
-        First name:
-        <input
-          value={player.firstName}
-          onChange={handleFirstNameChange}
-        />
-      </label>
-      <label>
-        Last name:
-        <input
-          value={player.lastName}
-          onChange={handleLastNameChange}
-        />
-      </label>
-    </>
-  );
+    return (
+        <>
+            <label>
+                Score: <b>{player.score}</b>
+                {' '}
+                <button onClick={handlePlusClick}>
+                    +1
+                </button>
+            </label>
+            <label>
+                First name:
+                <input
+                    value={player.firstName}
+                    onChange={handleFirstNameChange}
+                />
+            </label>
+            <label>
+                Last name:
+                <input
+                    value={player.lastName}
+                    onChange={handleLastNameChange}
+                />
+            </label>
+        </>
+    );
 }
